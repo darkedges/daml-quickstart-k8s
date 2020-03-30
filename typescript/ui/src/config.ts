bb3f4f2d35e633fcbd13e29d0d2d3637f14bb3aa
Nicholas Irving <nirving@darkedges.com>
2020-03-31 06:10:47 +1100

    updates to ensure it will compile correctly

--- typescript/ui/src/config.ts
+++ typescript/ui/src/config.ts
@@ -1,5 +1,5 @@
 export const ledgerId = 'MyLedger';
 
-export const httpBaseUrl = `https://quikstart-api.daml.darkedges.com/data/${ledgerId}/`;
+export const httpBaseUrl = `https://quickstart-api.daml.darkedges.com/data/${ledgerId}/`;
 
-export const wsBaseUrl = 'ws://quikstart-api.daml.darkedges.com/';
+export const wsBaseUrl = 'ws://quickstart-api.daml.darkedges.com/';
