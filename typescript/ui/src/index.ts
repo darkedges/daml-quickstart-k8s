1bbe13f6da1157c4037eb21931cb85c2d5fbf204
Nicholas Irving <nirving@darkedges.com>
2020-03-31 21:11:04 +1100

    Comments

--- typescript/ui/src/index.ts
+++ typescript/ui/src/index.ts
@@ -1,11 +1,21 @@
 import Ledger from '@daml/ledger';
 import { Credentials, computeCredentials } from './credentials';
 import { httpBaseUrl, wsBaseUrl } from './config';
-import { Iou, IouTrade } from '@ts/quickstart-0.0.1'
+import { Iou } from '@ts/quickstart-0.0.1'
 
 const credentials: Credentials = computeCredentials("Alice");
 
 const ledger = new Ledger({ token: credentials.token, httpBaseUrl, wsBaseUrl });
-ledger.lookupByKey(Iou, credentials.party)
+// const iou: Iou = {
+//     issuer: "Alice",
+//     owner: "Alice",
+//     currency: "AliceCoin",
+//     amount: "1.0",
+//     observers: []
+// };
+// ledger.create(Iou, iou)
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+ledger.query(Iou)
     .then(data => console.log(data))
     .catch(error => console.log(error));
