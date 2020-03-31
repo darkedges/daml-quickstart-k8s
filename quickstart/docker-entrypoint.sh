b7761b71d28d0e0333e0b9ebb879a239c7f8153d
DarkEdges <nirving.darkedges@darkedges.com>
2020-03-31 20:43:59 +1100

    Updated Access Token

--- quickstart/docker-entrypoint.sh
+++ quickstart/docker-entrypoint.sh
@@ -5,21 +5,20 @@ CMD="${1:-bash}"
 echo "Command is $CMD"
 
 sandbox() {
-    MY_POD_IP=$(hostname -i)
     cd quickstart
-    daml sandbox --address $MY_POD_IP --wall-clock-time --ledgerid MyLedger --auth-jwt-hs256-unsafe=secret .daml/dist/quickstart-0.0.1.dar
+    daml sandbox --address 0.0.0.0 --wall-clock-time --ledgerid MyLedger --auth-jwt-hs256-unsafe=secret .daml/dist/quickstart-0.0.1.dar
 }
 
 jsonapi() {
     MY_POD_IP=$(hostname -i)
     cd quickstart
-    daml json-api --address $MY_POD_IP --ledger-host quickstart-sandbox --ledger-port 6865 --http-port 7575 --access-token-file /home/daml/accessToken
+    daml json-api --address 0.0.0.0 --ledger-host quickstart-sandbox --ledger-port 6865 --http-port 7575 --access-token-file /home/daml/accessToken
 }
 
 navigator() {
     cd quickstart
-    daml script --dar .daml/dist/quickstart-0.0.1.dar --script-name Setup:initialize --ledger-host quickstart-sandbox --ledger-port 6865 --static-time
-    daml navigator server quickstart-sandbox 6865
+    daml script --dar .daml/dist/quickstart-0.0.1.dar --script-name Setup:initialize --ledger-host quickstart-sandbox --ledger-port 6865 --wall-clock-time --access-token-file /home/daml/accessToken
+    daml navigator server --access-token-file /home/daml/accessToken quickstart-sandbox 6865
 }
 
 case "$CMD" in
